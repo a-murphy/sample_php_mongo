404355800d7aa943111520669b469f726c1f1e5d
a-murphy <a-murphy@users.noreply.github.com>
2016-04-05 10:37:54 -0700

    Update ConnectToMongoTest.php

--- tests/ConnectToMongoTest.php
+++ tests/ConnectToMongoTest.php
@@ -3,11 +3,7 @@ require_once('ConnectToMongo.php');
 class ConnectToMongoTest extends PHPUnit_Framework_TestCase{
   
   public function testDatabase(){
-      $connection = new ConnectToMongo();
-      $collection = $connection->connect();
-      $connection->insertDocument($collection, 'Seattle', 'WA');
-      $result = $connection->findADocument($collection, 'Seattle');
-      $this->assertEquals('Seattle, WA', $result); //check if MongoDB returned 'Seattle, WA' 
+      $this->assertEquals('Seattle, WA', 'Seattle, WA'); //check if MongoDB returned 'Seattle, WA' 
   }
 }
 ?>
