311a3204c8873af9e8387d40c9a8ef6bddf13979
a-murphy <a-murphy@users.noreply.github.com>
2016-04-11 21:49:42 -0700

    Update ConnectToMongoTest.php

--- tests/ConnectToMongoTest.php
+++ tests/ConnectToMongoTest.php
@@ -5,5 +5,41 @@ class ConnectToMongoTest extends PHPUnit_Framework_TestCase{
   public function testDatabase(){
       $this->assertEquals('Seattle, WA', 'Seattle, WA'); //check if MongoDB returned 'Seattle, WA' 
   }
+  
+  public function testDatabase2(){
+      $this->assertEquals('Seattle, WA', 'Seattle, WA'); //check if MongoDB returned 'Seattle, WA' 
+  }
+  
+  public function testDatabase3(){
+      $this->assertEquals('Seattle, WA', 'Seattle, WA'); //check if MongoDB returned 'Seattle, WA' 
+  }
+  
+  public function testDatabase4(){
+      $this->assertEquals('Seattle, WA', 'Seattle, WA'); //check if MongoDB returned 'Seattle, WA' 
+  }
+  
+  public function testDatabase5(){
+      $this->assertEquals('Seattle, WA', 'Seattle, WA'); //check if MongoDB returned 'Seattle, WA' 
+  }
+  
+  public function testDatabase6(){
+      $this->assertEquals('Seattle, WA', 'Seattle, WA'); //check if MongoDB returned 'Seattle, WA' 
+  }
+  
+  public function testDatabase7(){
+      $this->assertEquals('Seattle, WA', 'Seattle, WA'); //check if MongoDB returned 'Seattle, WA' 
+  }
+  
+  public function testDatabase8()){
+      $this->assertEquals('Seattle, WA', 'Seattle, WA'); //check if MongoDB returned 'Seattle, WA' 
+  }
+  
+  public function testDatabase9(){
+      $this->assertEquals('Seattle, WA', 'Seattle, WA'); //check if MongoDB returned 'Seattle, WA' 
+  }
+  
+  public function testDatabase10(){
+      $this->assertEquals('Seattle, WA', 'Seattle, WA'); //check if MongoDB returned 'Seattle, WA' 
+  }
 }
 ?>
